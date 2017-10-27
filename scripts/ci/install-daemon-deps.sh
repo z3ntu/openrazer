35828cc391cab1960944e5153cc0a11ad253c7d0
Luca Weiss <luca@z3ntu.xyz>
2017-10-27 17:42:19 +0200

    Reorder Debian deps, download and install hidraw

--- scripts/ci/install-daemon-deps.sh
+++ scripts/ci/install-daemon-deps.sh
@@ -3,6 +3,11 @@
 # Ubuntus python3-daemonize version is outdated. Grab and install it manually.
 curl -L -O https://launchpad.net/~openrazer/+archive/ubuntu/daily/+files/python3-daemonize_2.4.7-xenial_all.deb
 dpkg -i python3-daemonize_2.4.7-xenial_all.deb
+curl -L -O https://launchpad.net/~z3ntu/+archive/ubuntu/experiments/+files/python3-ioctl-opt_1.2-0_all.deb
+dpkg -i python3-ioctl-opt_1.2-0_all.deb
+curl -L -O https://launchpad.net/~z3ntu/+archive/ubuntu/experiments/+files/python3-hidraw-pure_1.1-1_all.deb
+dpkg -i python3-hidraw-pure_1.1-1_all.deb
+
 apt-get -f -y install
 
 apt-get -y install \
